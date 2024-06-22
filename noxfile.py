15959dd7eff4e879ccaf2b4085a851d4e0eea34f
Shangjin Tang <shangjin.tang@gmail.com>
2024-06-22 16:04:42 +0800

    add 'black' and 'isort' in nox; not enabled by default

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,21 @@
 import nox
 
+nox.options.sessions = ["pytest"]
+
 
 @nox.session(tags=["test"])
 def pytest(session):
     session.install("pytest")
     session.run("pytest")
+
+
+@nox.session(tags=["fix"])
+def black(session):
+    session.install("black")
+    session.run("black", "src", "tests")
+
+
+@nox.session(tags=["fix"])
+def isort(session):
+    session.install("isort")
+    session.run("isort", "src", "tests")
